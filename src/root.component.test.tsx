304d7c6c8dbec8c1fb8f25287baeb3ed45c02680
Sam Carbine <scarbine@me.com>
2021-09-19 12:59:15 -0500

    removed boiler plate jest and added test to make sure the success message is rending in the Dom.

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,9 +1,12 @@
 import { render } from "@testing-library/react";
+import { SuccessLandingPage } from "./components/SuccessLandingPage";
 import Root from "./root.component";
 
-describe("Root component", () => {
+describe("Success Landing Page", () => {
   it("should be in the document", () => {
-    const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+    const { getByText } = render(<SuccessLandingPage />);
+    expect(
+      getByText(/you submitted the form successfully!/i)
+    ).toBeInTheDocument();
   });
 });
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -5,9 +5,7 @@ import { SuccessLandingPage } from "./components/SuccessLandingPage";
 export default function Root(props) {
   return (
     <>
-      <h5>MiniApp2</h5>
       <SuccessLandingPage />
-      <section>{props.name} is mounted!</section>
     </>
   );
 }
